d5765b51fe296918dc85777d0421b9a19839b585
warisamir <warisamir1918@gmail.com>
2024-05-22 20:46:25 +0530

    hewllo

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+
+const app = express();
+
+
+
+const PORT = 8000;
+
+
+app.listen(PORT, () =>{ console.log(`Your server is running successfully on PORT ${PORT}`)});
+app.listen(console.log("hello world"))
