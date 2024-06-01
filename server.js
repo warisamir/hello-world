06bbc13cacec13c0a85857ab0571252377954e17
WarisAmir <warisamir1918@gmail.com>
2024-06-01 15:21:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express from 'express';
 
-
 const app = express();
-
-
-
 const PORT = 8000;
-app,get('/',async(req,res)=>{
-  cosnole.log("heello world");
+
+app.get('/', (req, res) => {
+  console.log("Hello, World!");
+  res.send("Hello, World!");
 });
-app.listen(PORT, () =>{ console.log(`Your server is running successfully on PORT ${PORT}`)});
-app.listen(console.log("hello world"))
+
+app.listen(PORT, () => {
+  console.log(`Your server is running successfully on PORT ${PORT}`);
+});
